262283138c11a84f57e6d1e189fdc402b25064ad
Kaytlin Borel <kaytlinrb@gmail.com>
2017-04-21 12:39:24 -0700

    add condition to remove any nonalphaetical characters

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,18 @@
 class String
   define_method(:anagram) do |word|
-    sorted_input = self.downcase().split('').sort()
-    sorted_word = word.downcase().split('').sort()
-    reversed_word =word.downcase().reverse()
+    sorted_input = self.downcase().delete("^a-z").split('').sort()
+    sorted_word = word.downcase().delete("^a-z").split('').sort()
+    reversed_word =word.downcase().delete("^a-z").reverse()
+    user_input = self.downcase().delete("^a-z")
 
-    if self.scan(/[aeiouy]/).count <1
+    if user_input.scan(/[aeiouy]/).count <1
       "not a real word"
-    elsif self.downcase == reversed_word
+    elsif (sorted_word  & sorted_input).empty?
+      "antigram!"
+    elsif user_input.downcase == reversed_word
       "palindromes!"
     elsif sorted_input == sorted_word
       "anagrams!"
     end
-
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -14,4 +14,10 @@ describe('String#anagram') do
   it('check if words have a vowel') do
     expect(('qmtp').anagram('ptmq')).to(eq('not a real word'))
   end
+  it('check if words are antigrams') do
+    expect(('cat').anagram('dog')).to(eq('antigram!'))
+  end
+  it ('eliminate any nonalphabetical characters') do
+    expect(('@@@@@@@to').anagram('@@@@@@@ot')).to(eq('palindromes!'))
+  end
 end
